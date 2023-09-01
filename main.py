8b54c8168ac3b3886a4dea5dcae4ecf30815fded
ae453 <ae453@users.noreply.github.com>
2023-09-01 15:41:21 -0400

    Adding SQL Handling

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import atexit
+import datetime
 import mysql.connector
 import customtkinter
 from dotenv import load_dotenv
@@ -11,15 +13,48 @@ ENV_dbUser = os.getenv('DB_USER')
 ENV_dbDatabase = os.getenv('DB_DATABASE')
 ENV_dbPassword = os.getenv('DB_PASSWORD')
 
-class dbConnection():
-    def __init__(self):
-        self.db = mysql.connector.connect(user=ENV_dbUser,
-                                          password=ENV_dbPassword,
-                                          host=ENV_dbUser,
-                                          database=ENV_dbDatabase)
-    # def dbQuerry(self):
-    #     self.db.
+db = mysql.connector.connect(user=ENV_dbUser,
+                                  password=ENV_dbPassword,
+                                  host=ENV_dbUser,
+                                  database=ENV_dbDatabase)
+dbcursor = db.cursor()
 
+class dbConnection():
+    # def __init__(self):
+    #     self.db = mysql.connector.connect(user=ENV_dbUser,
+    #                                       password=ENV_dbPassword,
+    #                                       host=ENV_dbUser,
+    #                                       database=ENV_dbDatabase)
+    #     self.cursor = self.db.cursor()
+        # self.closecursor = self.cursor.close()
+        # self.closedb = self.db.close()
+    def dbAddItem(self, table: str, itemName: str, itemExpiration: datetime.datetime, itemNutritionalValue: dict):
+        addItemQuerry = (f"INSERT INTO {table}"
+                          "(id, itemName, itemExpiration, itemNutritionalValues)"
+                          "VALUES %(id)s %(itemName)s %(itemExpiration)s %(itemNutritionalValues)s")
+        id = dbcursor.lastrowid
+        addItemData = {
+            'id': id,
+            'itemName': itemName,
+            'itemExpiration': itemExpiration,
+            'itemNuritionalValue': itemNutritionalValue
+        }
+        dbcursor.execute(addItemQuerry, addItemData)
+
+        db.commit()
+        # self.cursor.close()
+        # self.db.close()
+
+def exit_handler():
+    dbcursor.close()
+    print("Database Cursor Closed!")
+    db.commit()
+    print("Unsaved Database Changes Committed!")
+    db.close()
+    print("Database Closed!")
+    print("Program Exiting!")
+
+atexit.register(exit_handler)
 if __name__ == "__main__":
     while True:
         item = input("What Item Do You Want to Input? ")
